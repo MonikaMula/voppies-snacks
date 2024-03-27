702b89ab297c835cc2c3a0eac0c96d5db88a9d27
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2024-03-27 13:58:16 +0000

    test error

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
-def index(request):
+
+def shop(request):    
+    return render(request, 'shop.html')
+def find(request):
+    return render(request, 'find.html')
+
     """ A view to return the index page """
-    
+ 
+def index(request):   
     return render(request, 'home/index.html')
